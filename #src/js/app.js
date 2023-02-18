a94095e0e72f3129ad3610660e1281feac4493ea
Spacespire <aleksandra@spacespire.com>
2023-02-18 10:12:51 +0200

    added grow-up btn

--- #src/js/app.js
+++ #src/js/app.js
@@ -240,6 +240,7 @@ class App {
 	componentsBeforeLoad() {
 		@@include('../common/promo-header/promo-header.js');
 		@@include('../common/hidden-list/hidden-list.js');
+		@@include('../common/growup-btn/growup-btn.js');
 	}
 
 	componentsAfterLoad() {
